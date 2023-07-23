7ac737df64e980a87264e92bcb39b019fcaff529
Subramanya K S <subramanyaks2205@gmail.com>
2023-07-23 16:42:56 +0530

    updated app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { StudentService } from './student/student.service';
+import { StudentController } from './student/student.controller';
+import { StudentSchema } from './schema/student.schema';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGODB_URL),
+    MongooseModule.forFeature([{ name: 'Student', schema: StudentSchema }]),
+    ConfigModule.forRoot(),
+  ],
+  controllers: [AppController, StudentController],
+  providers: [AppService, StudentService],
 })
 export class AppModule {}
